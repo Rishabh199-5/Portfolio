711cef28280ea5509d4908f27eb646d2d73757d3
Rishabh199-5 <dangwalrishabh@gmail.com>
2020-01-17 00:54:50 +0530

    changes for project section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,44 +1,47 @@
 import { Component} from '@angular/core';
-
+import { Constants } from 'src/common/constants';
+import { stringify } from 'querystring';
 @Component({
   selector: 'projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-  filters:Array<{name:string,children:Array<{title:string,subTitle:string,imgUrl:string}>}>;
+  filters:Array<{name:string,type:number}>;
   selectedIndex=0;
+  projects:Array<any>=[];
+  currentProjects:Array<{imageUrl:string,title:string}>;
   constructor() { 
     this.filters=[];
+    this.projects=[];
   }
 
   ngOnInit() {
     this.filters=[
       {
-        name:"ALL",children:[
-          
-        ]
+        name:"ALL",type:Constants.FILTER_TYPES.ALL
       },
       {
-        name:"ANGULAR",children:[
-          
-        ]
+        name:"ANGULAR",type:Constants.FILTER_TYPES.ANGULAR
       },
       {
-        name:"JAVASCRIPT",children:[
-          
-        ]
+        name:"JAVASCRIPT",type:Constants.FILTER_TYPES.JS
       },
       {
-        name:"NODEJS",children:[
-          
-        ]
+        name:"NODEJS",type:Constants.FILTER_TYPES.NODEJS
+      }
+    ]
+
+    this.projects=[
+      {
+        title:"Assignment Real Time Review",imgUrl:"assets/pics/ARR.svg",type:Constants.FILTER_TYPES.NODEJS
       }
     ]
   }
 
   activeFilter(index:number){
     this.selectedIndex=index;
+
   }
 
 }
---
+++ src/common/constants.ts
@@ -0,0 +1,8 @@
+export class Constants{
+    public static FILTER_TYPES=class{
+        public static ALL=1;
+        public static ANGULAR=2;
+        public static JS=3;
+        public static NODEJS=4;
+    }
+}
