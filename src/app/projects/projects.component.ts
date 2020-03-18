e7db876839c4a86110ad2ff00e0c93e336c7141f
Rishabh199-5 <dangwalrishabh@gmail.com>
2020-03-18 23:54:49 +0530

    projects changes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,10 +10,11 @@ export class ProjectsComponent {
   filters:Array<{name:string,type:number}>;
   selectedIndex=0;
   projects:Array<any>=[];
-  currentProjects:Array<{imageUrl:string,title:string}>;
+  currentProjects:Array<{imageUrl:string,title:string,tech:string}>;
   constructor() { 
     this.filters=[];
     this.projects=[];
+    this.currentProjects=[];
   }
 
   ngOnInit() {
@@ -26,35 +27,35 @@ export class ProjectsComponent {
       },
       {
         name:"JAVASCRIPT",type:Constants.FILTER_TYPES.JS
-      },
-      {
-        name:"NODEJS",type:Constants.FILTER_TYPES.NODEJS
       }
     ]
 
     this.projects=[
       {
-        title:"Real Time Application",imgUrl:"assets/pics/ARR.svg",type:Constants.FILTER_TYPES.NODEJS
+        title:"Real Time Application",imgUrl:"assets/pics/collab.png",type:Constants.FILTER_TYPES.JS,tech:"JS / ANGULAR"
       },
       {
-        title:"Admissions",imgUrl:"assets/pics/admissions.jpg",type:Constants.FILTER_TYPES.ANGULAR,
+        title:"Admissions",imgUrl:"assets/pics/admissions.jpg",type:Constants.FILTER_TYPES.ANGULAR,tech:"ANGULAR/HTML5/SASS"
       },
       {
-        title:"Ecommerce",imgUrl:"assets/pics/ecomm.png",type:Constants.FILTER_TYPES.ANGULAR,
+        title:"Ecommerce",imgUrl:"assets/pics/ecomm.png",type:Constants.FILTER_TYPES.ANGULAR,tech:"ANGULAR/HTML5/SASS"
       },
       {
-        title:"Point Of Sale",imgUrl:"assets/pics/posjpg.jpg",type:Constants.FILTER_TYPES.ANGULAR,
+        title:"Point Of Sale",imgUrl:"assets/pics/posjpg.jpg",type:Constants.FILTER_TYPES.ANGULAR,tech:"ANGULAR/HTML5/CSS3"
       },{
-        title:"Websites",imgUrl:"assets/pics/web.jpg",type:Constants.FILTER_TYPES.ANGULAR,
-
+        title:"Websites",imgUrl:"assets/pics/web.jpg",type:Constants.FILTER_TYPES.ANGULAR,tech:"ANGULAR/HTML5/CSS3"
       }
-
     ]
+    this.currentProjects=JSON.parse(JSON.stringify(this.projects));
   }
 
   activeFilter(index:number){
     this.selectedIndex=index;
-
+    if(this.filters[this.selectedIndex].type==Constants.FILTER_TYPES.ALL){
+      this.currentProjects=JSON.parse(JSON.stringify(this.projects));
+    }else{
+      this.currentProjects=this.projects.filter(ele=>ele.type==this.filters[this.selectedIndex].type)
+    }
   }
 
 }
